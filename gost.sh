ebf83bee8d04b3d3a1e0d701aa73612cc5711fa0
elbao <65481904+elbao@users.noreply.github.com>
2020-05-19 18:43:36 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 echo "本脚本仅供学习交流，请勿用于其他用途，否则后果自负"
-
 file_name=gost-linux-amd64-2.11.0.gz
 if [[ ! -f $file_name ]]; then
   yum install wget -y
@@ -8,7 +7,6 @@ if [[ ! -f $file_name ]]; then
 fi
 gzip -d gost-linux-amd64-2.11.0.gz && mv gost* gost && chmod +x gost
 echo "gost安装成功"
-
 read -p "请输入要中转的服务器IP:" dest
 read -p "请输入要转发的起始端口号:" start_port
 read -p "请输入要转发的终止端口号:" end_port
